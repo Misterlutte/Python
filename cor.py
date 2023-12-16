b50d7164bd0cd777b8ba0a688a50336030322471
EbatKopat <nzagnetov@bk.ru>
2023-12-16 12:50:17 +0300

    smt

---
+++ cor.py
@@ -0,0 +1,29 @@
+# n = int(input())
+# a = n // 100
+# b = n % 100 // 10
+# c = n % 10
+# print(f'{a + b + c} ({a} + {b} + {c})')
+
+
+
+# m = int(input())
+# n = int(input())
+# k = int(input())
+# if k % m == 0 or k % n == 0:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# n = int(input())
+# a = n // 100000 + n % 100000 //10000 + n % 10000 // 1000
+# b = n % 1000 // 100 + n % 100 // 10 + n % 10
+# if a == b:
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+# s = int(input())
+# print(int(s / 6), int(s * 2 / 3), int(s / 6))
